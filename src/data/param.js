e7855c9a96dd8810d181bc0efe5ba7ed0ed69f64
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-03-26 01:43:59 +0900

    chore: param obj key 수정

--- src/data/param.js
+++ src/data/param.js
@@ -1,6 +1,6 @@
 export const userParam = {
-  userName: "홍길동",
-  id: "hong@example.com",
-  pw: "1234",
-  description: "안녕하세요, 항해플러스에서 열심히 공부하고 있는 홍길동입니다.",
+  username: "홍길동",
+  email: "hong@example.com",
+  password: "1234",
+  bio: "안녕하세요, 항해플러스에서 열심히 공부하고 있는 홍길동입니다.",
 };
