290b6366cf86ca34e68d649fb770f253d8a57dfe
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-03-26 22:04:12 +0900

    update: 새로고침 방지를 위해 이벤트 위임 사용하기

--- src/routes.js
+++ src/routes.js
@@ -24,15 +24,14 @@ export const renderPage = (path) => {
 export const render = () => {
   renderPage(location.pathname);
 
-  document.querySelectorAll("a").forEach((el) => {
-    el.addEventListener("click", (e) => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.tagName === "A") {
       e.preventDefault();
-
       const newPathName = e.target.href.replace(location.origin, "");
       history.pushState(null, null, newPathName);
 
       renderPage(newPathName);
-    });
+    }
   });
 };
 
