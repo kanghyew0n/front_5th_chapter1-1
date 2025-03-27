39c1e291908253a9fb1fc0f6498211efd5c1846e
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-03-28 02:25:03 +0900

    refactor: 싱글톤 패턴 적용

--- src/store/store.js
+++ src/store/store.js
@@ -1,37 +1,54 @@
-import { userParam } from "../data/param";
 import { USER_STORAGE_KEY } from "../constants";
 
-export const userStore = {
-  init: () => {
-    localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(userParam));
-  },
-  setUserInfo: (userInfo) => {
-    const { username, email, bio } = userInfo;
-    const originUserInfo = userStore.getUserInfo();
+let instance = null;
 
-    // 로그인 시
-    if (!originUserInfo) {
-      localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(userInfo));
-      return;
-    }
+const createUserStore = () => {
+  if (instance) {
+    return instance;
+  }
 
-    // 프로필 수정 시
-    const newUserInfo = {
-      ...originUserInfo,
-      ...(originUserInfo.username !== username && { username }),
-      ...(originUserInfo.email !== email && { email }),
-      ...(originUserInfo.bio !== bio && { bio }),
-    };
+  const userStore = {
+    userInfo: JSON.parse(localStorage.getItem(USER_STORAGE_KEY)),
 
-    localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(newUserInfo));
-  },
-  getUserInfo: () => {
-    return JSON.parse(localStorage.getItem(USER_STORAGE_KEY));
-  },
-  loggedIn: () => {
-    return !!userStore.getUserInfo();
-  },
-  logout: () => {
-    localStorage.removeItem(USER_STORAGE_KEY);
-  },
+    setUserInfo(userInfo) {
+      const originUserInfo = this.userInfo;
+
+      // 로그인 할 경우
+      if (!originUserInfo) {
+        this.userInfo = userInfo;
+        return localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(userInfo));
+      }
+
+      // 프로필 업데이트 할 경우
+      const { username, email, bio } = userInfo;
+      const newUserInfo = {
+        ...originUserInfo,
+        ...(originUserInfo.username !== username && { username }),
+        ...(originUserInfo.email !== email && { email }),
+        ...(originUserInfo.bio !== bio && { bio }),
+      };
+
+      this.userInfo = newUserInfo;
+      localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(newUserInfo));
+    },
+
+    getUserInfo() {
+      return this.userInfo;
+    },
+
+    loggedIn() {
+      return !!this.userInfo;
+    },
+
+    logout() {
+      this.userInfo = null;
+      localStorage.removeItem(USER_STORAGE_KEY);
+    },
+  };
+
+  instance = userStore;
+  return userStore;
 };
+
+const userStore = createUserStore();
+export { userStore };
